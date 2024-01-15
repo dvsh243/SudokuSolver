fb40319ab50a7c21124506eed6d757a6cbe2c79d
Devesh Ahuja <ahujadevesh1@gmail.com>
2024-01-15 21:19:10 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+startingSudoku = """
+                    024007000
+                    600000000
+                    003680415
+                    431005000
+                    500000032
+                    790000060
+                    209710800
+                    040093000
+                    310004750
+                """
+
+
+def PrintSudoku(sudoku):
+    print("\n")
+    for i in range(len(sudoku)):
+        line = ""
+        if i == 3 or i == 6:
+            print("---------------------")
+        for j in range(len(sudoku[i])):
+            if j == 3 or j == 6:
+                line += "| "
+            line += str(sudoku[i][j])+" "
+        print(line)
+
+
+sudoku = [ 
+    [int(i) for i in line] 
+    for line in startingSudoku.split()
+]
+PrintSudoku(sudoku)
