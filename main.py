05a6c764e0448e8556bdb9e87c54f2c9b49a9f02
Devesh Ahuja <ahujadevesh1@gmail.com>
2024-01-16 12:09:07 +0530

    SudokuSolver class

--- main.py
+++ main.py
@@ -1,33 +1,56 @@
-import numpy as np
+class SudokuSolver:
 
-startingSudoku = """
-                    024007000
-                    600000000
-                    003680415
-                    431005000
-                    500000032
-                    790000060
-                    209710800
-                    040093000
-                    310004750
-                """
+    def __init__(self, startingSudoku: str) -> None:
+    
+        self.sudoku = [ 
+            [int(i) for i in line] 
+            for line in startingSudoku.split()
+        ]
+        
+        self.solve()
 
 
-def PrintSudoku(sudoku):
-    print("\n")
-    for i in range(len(sudoku)):
-        line = ""
-        if i == 3 or i == 6:
-            print("---------------------")
-        for j in range(len(sudoku[i])):
-            if j == 3 or j == 6:
-                line += "| "
-            line += str(sudoku[i][j])+" "
-        print(line)
+    @staticmethod
+    def PrintSudoku(sudoku):
+        print("\n")
+
+        for i in range(len(sudoku)):
+            line = ""
+            if i == 3 or i == 6:
+                print("---------------------")
+            for j in range(len(sudoku[i])):
+                if j == 3 or j == 6:
+                    line += "| "
+                line += str(sudoku[i][j]) + " "
+            print(line)
+
+        print("\n")
 
 
-sudoku = [ 
-    [int(i) for i in line] 
-    for line in startingSudoku.split()
-]
-PrintSudoku(sudoku)
+    def solve(self):
+
+        self.PrintSudoku(self.sudoku)
+
+    
+    def fillBlock(self):
+        pass
+
+
+
+
+
+if __name__ == "__main__":
+
+    startingSudoku = """
+                        024007000
+                        600000000
+                        003680415
+                        431005000
+                        500000032
+                        790000060
+                        209710800
+                        040093000
+                        310004750
+                    """
+    
+    SudokuSolver(startingSudoku)
