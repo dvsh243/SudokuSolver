a7d147354fabb46ebbbcc1c9d08c0976ddb843a4
Devesh Ahuja <ahujadevesh1@gmail.com>
2024-01-16 12:25:20 +0530

    random numbers generated

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 class SudokuSolver:
 
     def __init__(self, startingSudoku: str) -> None:
@@ -8,6 +10,8 @@ class SudokuSolver:
         ]
         
         self.solve()
+
+        print("\n")
 
 
     @staticmethod
@@ -23,20 +27,55 @@ class SudokuSolver:
                     line += "| "
                 line += str(sudoku[i][j]) + " "
             print(line)
-
+            
         print("\n")
 
 
     def solve(self):
 
         self.PrintSudoku(self.sudoku)
+        self.getRandomState()
 
     
-    def fillBlock(self):
-        pass
+    def getRandomState(self):
+        
+        # `x` & `y` are block indexes.
+        for x in range(3):
+            for y in range(3):
+                print(f"filling block[{x}][{y}]...")
+                self.fillBlock(x, y)
+                return
 
 
+    def fillBlock(self, x: int, y: int):
+        """ranges --- r -> [{x*3}, {x*3+2}]    c -> [{y*3}, {y*3+2}]"""
 
+        toFill, unique = 0, set()
+
+        for r in range(x*3, x*3+3):
+            for c in range(y*3, y*3+3):
+                if not self.sudoku[r][c]: 
+                    toFill += 1
+                    continue
+                unique.add( self.sudoku[r][c] )
+
+        print(f"unique -> {unique} | toFill = {toFill}")
+        # choosing `toFill` random numbers from [1, 9] that don't exist in `unique`
+        print(f"recieved randoms -> { self.getRandom(toFill, unique) }")
+
+
+    @staticmethod
+    def getRandom(count: int, unique: set) -> set:
+        """returns `count` random numbers from [1, 9] that dont exist in `unique`"""
+        
+        numbers = []
+        while len(unique) < 9:
+            n = random.randint(1, 9)
+            if n not in unique: 
+                numbers.append(n)
+                unique.add(n)
+        
+        return numbers
 
 
 if __name__ == "__main__":
